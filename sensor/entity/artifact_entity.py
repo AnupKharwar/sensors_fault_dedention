ffff55f6f430d0eb9abc60d2a3c5c0846f01ce55
anupkharwar <anup.kharwar@gmail.com>
2022-11-27 16:39:51 +0530

    Update

---
+++ sensor/entity/artifact_entity.py
@@ -0,0 +1,6 @@
+class DataIngestionConfigArtifact
+class DataValidationArtifact
+class DataTrasformationArtifact
+class ModelTrainingArtifact
+class ModelEvaluationArtifact
+class ModelPusherArtificat
